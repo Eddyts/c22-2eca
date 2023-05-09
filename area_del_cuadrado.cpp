671167cc5377383aeee0601c4df053d08b42bf3b
Eduardo Garcia <128424577+Eddyts@users.noreply.github.com>
2023-05-08 22:21:49 -0600

    Create area_del_cuadrado.cpp

---
+++ area_del_cuadrado.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+int main (){
+	//declaracion de variables 
+	
+	float lado;
+	float area;
+
+	cout << "ingresa el valor de el lado de el cuadraro: ";
+
+	cin >> lado;
+
+	area = lado * lado;
+
+	cout << "el lado es igual a " << area <<'\n';
+	
+	cout << "hecho por Garcia Cortes" << '\n';
+
+	return 0;
+}
